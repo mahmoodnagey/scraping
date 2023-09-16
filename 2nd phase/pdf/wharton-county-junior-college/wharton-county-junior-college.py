cea153cff7e0db4aad786c46b6feb40a7ce20c53
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2023-09-16 10:28:06 +0300

    wharton-county-junior-college

--- 2nd phase/pdf/wharton-county-junior-college/wharton-county-junior-college.py
+++ 2nd phase/pdf/wharton-county-junior-college/wharton-county-junior-college.py
@@ -1,1 +1,30 @@
-# Note: first digit after course code is the credit hours, '4' in this 4:3:2
+import re
+import csv
+
+def read_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
+
+text = read_file('wharton-county-junior-college-source.txt')
+
+# Regular expression pattern to capture code, credit, title, and description
+pattern = re.compile(r'(?P<code>[A-Z]+\s+\d+)\s+(\d+:\d+:\d+)\n(?P<title>[^\n]+)\n(.*?)(?=(?:[A-Z]+\s+\d+|$))', re.DOTALL)
+
+matches = list(pattern.finditer(text))
+
+# Prepare data for CSV
+csv_data = []
+for match in matches:
+    code = match.group('code')
+    credit = match.group(2).split(':')[0]
+    title = match.group('title')
+    description = match.group(4).strip()
+    csv_data.append([code, title, credit, description])
+
+# Write to CSV
+with open('wharton-county-junior-college.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['code', 'title', 'credit', 'description'])  # headers
+    writer.writerows(csv_data)
+
+print("Data exported to wharton-county-junior-college.csv")
